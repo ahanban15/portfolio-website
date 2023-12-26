180da548fe09fff7904fd191ea63708d5fc8842c
Ahan Bandyopadhyay <ahanbandyopadhyay@gmail.com>
2023-12-26 19:21:21 +0530

    Commit on sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import Navbar from "./Components/Navbar/Navbar";
 import "./app.scss"
 import Test from './Test';
+import Sidebar from './Components/Sidebar/Sidebar';
 
 const App = () => {
   return <div>
-    <section>
+    <section id="Homepage">
       <Navbar/>
     </section>
-    <section>Parallax</section>
+    <section id="About">Parallax</section>
     <section>About</section>
-    <section>Parallax</section>
+    <section id="Projects">Parallax</section>
     <section>Projects</section>
-    <section>Parallax</section>
+    <section id = "Contact">Parallax</section>
     <section>Contact</section>
   {/* <Test/>
   <Test/> */}

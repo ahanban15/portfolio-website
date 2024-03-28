6f26108a852902f32dcab59c8b08b1cb5ff33bec
Ahan Bandyopadhyay <ahanbandyopadhyay@gmail.com>
2024-03-28 10:37:37 +0530

    frontend edits

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -1,13 +1,21 @@
-import sanityClient from '@sanity/client';
+// import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
+import {createClient} from '@sanity/client'
 
-export const client = sanityClient({
+// export const client = sanityClient({
+//     projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+//     dataset: 'production',
+//     apiVersion: '2022-02-01',
+//     useCdn: true,
+//     token: process.env.REACT_APP_SANITY_TOKEN,
+// });
+export const client = createClient({
     projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
-    apiVersion: '2022-02-01',
-    useCdn: true,
-    token: process.env.REACT_APP_SANITY_TOKEN,
-});
+    useCdn: true, // set to `false` to bypass the edge cache
+    apiVersion: '2024-03-28', // use current date (YYYY-MM-DD) to target the latest API version
+    token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
+  })
 
 const builder = imageUrlBuilder(client);
 
